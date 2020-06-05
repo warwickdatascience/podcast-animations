21cd60f3038b65d7b6d2122e9630e191df8f5914
THargreaves <tim.hargreaves@icloud.com>
2020-06-05 17:41:33 +0100

    Add multi-class state with colours

--- rule_30/rule_30.py
+++ rule_30/rule_30.py
@@ -16,11 +16,12 @@ import tqdm
 # Parameters
 VIDEO_WIDTH = 1920
 VIDEO_HEIGHT = 1080
-SECS = int(60 * 1)
-FPS = 30
-PIXEL_SIZE = 2
+SECS = int(60 * 2)
+FPS = 10
+PIXEL_SIZE = 4
 OUTPUT_PATH = 'rule_30.mp4'
 FFMPEG_PATH = '/usr/bin/ffmpeg'
+COMP_LEVEL = 3  # trade-off speed and temp storage requirements
 
 # Constants
 STATE_WIDTH = VIDEO_WIDTH // PIXEL_SIZE
@@ -32,8 +33,14 @@ class Rule30:
     """A class for running Rule 30."""
 
     neighbours = np.array([[1, 2, 4]], np.uint8)
-    kernel = np.array([0, 1, 1, 1, 1, 0, 0, 0])
-    colours = np.array([[0, 0, 0], [255, 255, 255]], np.uint8)
+    kernel = np.array([0, 1, 2, 3, 4, 0, 0, 0])
+    colours = np.array([
+        [0, 0, 0],
+        [28, 19, 100],
+        [19, 46, 125],
+        [0, 108, 182],
+        [178, 196, 255],
+    ], np.uint8)
 
     def __init__(self, width, height):
         self.width = width
@@ -41,7 +48,7 @@ class Rule30:
 
         self.state = np.zeros((self.height, self.width), np.uint8)
         self.peak_height = 1
-        self.state[-1, self.width // 2] = 1
+        self.state[-1, self.width // 2] = 2
 
         self.rgb = None
         self._update_rgb()
@@ -51,17 +58,19 @@ class Rule30:
         self._update_rgb()
     
     def _update_state(self):
-        rule_index = sg.convolve2d(self.state[-1, None, :], self.neighbours,
+        conv_row_alive = (self.state[-1, None, :] > 0).astype(np.uint8)
+        rule_index = sg.convolve2d(conv_row_alive, self.neighbours,
                                    mode='same', boundary='wrap')
         new_row = self.kernel[rule_index]
         self.state = np.concatenate((self.state[1:], new_row))
 
         if self.peak_height < self.height:
             self.peak_height += 1
-            self.state[-self.peak_height, self.width // 2] = 1
+            self.state[-self.peak_height, self.width // 2] = 2
 
     def _update_rgb(self):
         self.rgb = self.colours[self.state]
+
 
 class VideoConverter:
     """A class for converting frames of NumPy arrays to a video."""
@@ -85,7 +94,7 @@ class VideoConverter:
                 dimensions.
         """
         frame_path = os.path.join(self.tmp_dir.name, f'{self.curr_frame}.png')
-        imageio.imwrite(frame_path, frame, compress_level=0)
+        imageio.imwrite(frame_path, frame, compress_level=COMP_LEVEL)
         self.curr_frame += 1
 
     def write(self, output_path):
